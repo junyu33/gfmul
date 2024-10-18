dfc9a496a8ca18753c61d78dc3c30f8942baa2b9
junyu33 <2658799217@qq.com>
2024-10-18 15:20:09 +0800

    chore: convert indents to spaces

--- gfmul.cpp
+++ gfmul.cpp
@@ -23,24 +23,24 @@ void print128(__m128i var)
 #ifdef __x86_64__
 __attribute__((target("sse2,pclmul")))
 inline void mul128(__m128i a, __m128i b, __m128i *res1, __m128i *res2) {
-	__m128i tmp3, tmp4, tmp5, tmp6;
-	tmp3 = _mm_clmulepi64_si128(a, b, 0x00);
-	tmp4 = _mm_clmulepi64_si128(a, b, 0x10);
-	tmp5 = _mm_clmulepi64_si128(a, b, 0x01);
-	tmp6 = _mm_clmulepi64_si128(a, b, 0x11);
+    __m128i tmp3, tmp4, tmp5, tmp6;
+    tmp3 = _mm_clmulepi64_si128(a, b, 0x00);
+    tmp4 = _mm_clmulepi64_si128(a, b, 0x10);
+    tmp5 = _mm_clmulepi64_si128(a, b, 0x01);
+    tmp6 = _mm_clmulepi64_si128(a, b, 0x11);
 
-	tmp4 = _mm_xor_si128(tmp4, tmp5);
-	tmp5 = _mm_slli_si128(tmp4, 8);
-	tmp4 = _mm_srli_si128(tmp4, 8);
-	tmp3 = _mm_xor_si128(tmp3, tmp5);
-	tmp6 = _mm_xor_si128(tmp6, tmp4);
-	// initial mul now in tmp3, tmp6
-	*res1 = tmp3;
-	*res2 = tmp6;
+    tmp4 = _mm_xor_si128(tmp4, tmp5);
+    tmp5 = _mm_slli_si128(tmp4, 8);
+    tmp4 = _mm_srli_si128(tmp4, 8);
+    tmp3 = _mm_xor_si128(tmp3, tmp5);
+    tmp6 = _mm_xor_si128(tmp6, tmp4);
+    // initial mul now in tmp3, tmp6
+    *res1 = tmp3;
+    *res2 = tmp6;
 }
 #elif __aarch64__
 inline void mul128(__m128i a, __m128i b, __m128i *res1, __m128i *res2) {
-	__m128i tmp3, tmp4, tmp5, tmp6;
+    __m128i tmp3, tmp4, tmp5, tmp6;
     poly64_t a_lo = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(a), 0);
     poly64_t b_lo = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(b), 0);
     poly64_t a_hi = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(a), 1);
@@ -124,32 +124,32 @@ inline void mul128(__m128i a, __m128i b, __m128i *res1, __m128i *res2) {
 #ifdef __x86_64__
 __attribute__((target("sse2,pclmul")))
 inline void gfmul (__m128i a, __m128i b, __m128i *res){
-	__m128i tmp3, tmp6, tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
-	__m128i XMMMASK = _mm_setr_epi32(0xffffffff, 0x0, 0x0, 0x0);
-	mul128(a, b, &tmp3, &tmp6);
-	tmp7 = _mm_srli_epi32(tmp6, 31);
-	tmp8 = _mm_srli_epi32(tmp6, 30);
-	tmp9 = _mm_srli_epi32(tmp6, 25);
-	tmp7 = _mm_xor_si128(tmp7, tmp8);
-	tmp7 = _mm_xor_si128(tmp7, tmp9);
-	tmp8 = _mm_shuffle_epi32(tmp7, 147);
+    __m128i tmp3, tmp6, tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
+    __m128i XMMMASK = _mm_setr_epi32(0xffffffff, 0x0, 0x0, 0x0);
+    mul128(a, b, &tmp3, &tmp6);
+    tmp7 = _mm_srli_epi32(tmp6, 31);
+    tmp8 = _mm_srli_epi32(tmp6, 30);
+    tmp9 = _mm_srli_epi32(tmp6, 25);
+    tmp7 = _mm_xor_si128(tmp7, tmp8);
+    tmp7 = _mm_xor_si128(tmp7, tmp9);
+    tmp8 = _mm_shuffle_epi32(tmp7, 147);
 
-	tmp7 = _mm_and_si128(XMMMASK, tmp8);
-	tmp8 = _mm_andnot_si128(XMMMASK, tmp8);
-	tmp3 = _mm_xor_si128(tmp3, tmp8);
-	tmp6 = _mm_xor_si128(tmp6, tmp7);
-	tmp10 = _mm_slli_epi32(tmp6, 1);
-	tmp3 = _mm_xor_si128(tmp3, tmp10);
-	tmp11 = _mm_slli_epi32(tmp6, 2);
-	tmp3 = _mm_xor_si128(tmp3, tmp11);
-	tmp12 = _mm_slli_epi32(tmp6, 7);
-	tmp3 = _mm_xor_si128(tmp3, tmp12);
+    tmp7 = _mm_and_si128(XMMMASK, tmp8);
+    tmp8 = _mm_andnot_si128(XMMMASK, tmp8);
+    tmp3 = _mm_xor_si128(tmp3, tmp8);
+    tmp6 = _mm_xor_si128(tmp6, tmp7);
+    tmp10 = _mm_slli_epi32(tmp6, 1);
+    tmp3 = _mm_xor_si128(tmp3, tmp10);
+    tmp11 = _mm_slli_epi32(tmp6, 2);
+    tmp3 = _mm_xor_si128(tmp3, tmp11);
+    tmp12 = _mm_slli_epi32(tmp6, 7);
+    tmp3 = _mm_xor_si128(tmp3, tmp12);
 
-	*res = _mm_xor_si128(tmp3, tmp6);
+    *res = _mm_xor_si128(tmp3, tmp6);
 }
 #elif __aarch64__
 inline void gfmul(__m128i a, __m128i b, __m128i *res){
-	uint32x4_t tmp3, tmp6, tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
+    uint32x4_t tmp3, tmp6, tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
     uint8x16_t Tmp3, Tmp6;
     uint32x4_t XMMMASK = vdupq_n_u32(0xffffffff);
     mul128(a, b, &Tmp3, &Tmp6);
@@ -269,7 +269,7 @@ int main() {
 
 /*
  * inline void mul128_v2(__m128i a, __m128i b, __m128i *res1, __m128i *res2) {
-	uint32x4_t tmp3, tmp4, tmp5, tmp6;
+    uint32x4_t tmp3, tmp4, tmp5, tmp6;
     poly64_t a_lo = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(a), 0);
     poly64_t b_lo = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(b), 0);
     poly64_t a_hi = (poly64_t)vgetq_lane_u64(vreinterpretq_u64_u8(a), 1);
